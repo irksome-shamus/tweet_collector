f8415f083cba5e08fd6fc1959e3fc206617fc0a2
Bradyn Glines <bradyngames@gmail.com>
2013-12-12 20:08:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetCollector::Application.config.secret_token = 'bb81016493b90aeb906f1c43ab3a3a81e04634527809a6cc429ba7f788cb04a8cb3c4c59e73a595a2126f0ffc134c4a233a75f1f7450057d4d75664ab8d544f4'
